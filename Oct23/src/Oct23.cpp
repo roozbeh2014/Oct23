addc64f5b01b0d5c8e8a4db2c19208a3f71a707d
roozbeh <roozbeh.manshaei@ryerson.ca>
2014-10-23 14:20:31 -0400

    I have 2 new variables.

---
+++ Oct23/src/Oct23.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Oct23.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 2;
+	int b = 3;
+	cout << a + b;
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
